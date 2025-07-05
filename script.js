8684d10446bb3a8bed595ddb0ed38bd077a0a9d2
MASKBOI7 <146939162+MASKBOI7@users.noreply.github.com>
2025-07-05 15:30:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+function showLogin() {
+  document.getElementById('form-box').innerHTML = `
+    <input type="text" id="username" placeholder="Username" />
+    <input type="password" id="password" placeholder="Password" />
+    <button onclick="login()">Login</button>
+  `;
+}
+
+function showSignup() {
+  document.getElementById('form-box').innerHTML = `
+    <input type="text" id="newUsername" placeholder="Create Username" />
+    <input type="password" id="newPassword" placeholder="Create Password" />
+    <button onclick="signup()">Sign Up</button>
+  `;
+}
+
+function signup() {
+  const u = document.getElementById('newUsername').value;
+  const p = document.getElementById('newPassword').value;
+  if (u && p) {
+    localStorage.setItem(u, p);
+    document.getElementById("msg").innerText = "Account created!";
+  } else {
+    document.getElementById("msg").innerText = "Please fill all fields.";
+  }
+}
+
+function login() {
+  const u = document.getElementById('username').value;
+  const p = document.getElementById('password').value;
+  const stored = localStorage.getItem(u);
+  if (stored === p) {
+    localStorage.setItem("currentUser", u);
+    window.location.href = "dashboard.html";
+  } else {
+    document.getElementById("msg").innerText = "Invalid login.";
+  }
+}
